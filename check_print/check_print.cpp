1e0debf0ffd95afafe0025e14b8d114377eaeaf7
ALEIVAN@live.spbstu.ru <ALEIVAN@live.spbstu.ru>
2024-04-17 16:15:33 +0300

    Добавьте файлы проекта.

---
+++ check_print/check_print.cpp
@@ -0,0 +1,77 @@
+﻿
+#include <iostream>
+#include <string>
+//#include <stack>
+
+int main()
+{
+    std::string a, b,c;
+    std::getline(std::cin, a);
+    std::getline(std::cin, b);
+    auto res_it = c.begin();
+
+    for (auto i = b.begin(); i != b.end(); i++)
+    {
+        if (*i == '<')
+        {
+            std::string s;
+            auto j = i+1;
+            while (*j != '>'&&j!=b.end())
+            {
+                s += *j;
+                j++;
+            }
+            if (s == "delete")
+            {
+                if (res_it != c.end()) res_it = c.erase(res_it);
+                i = b.erase(i, i + 8);
+                i--;
+            }
+            else if (s == "bspace")
+            {
+                if(res_it!=c.begin()) res_it= c.erase(res_it-1);
+                i = b.erase(i, i + 8);
+                i--;
+            }
+            else if (s == "left")
+            {
+                i = b.erase(i, i + 6);
+                if(res_it!=c.begin()) res_it--;
+                i--;
+            }
+            else if (s == "right")
+            {
+                i = b.erase(i, i + 7);
+                if (res_it != c.end()) res_it++;
+                i--;
+            }
+            else
+            {
+                b += '<' + s;
+                c += '<' + s;
+                i = j;
+            }
+
+        }
+        else
+        {
+            if (res_it == c.end())
+            {
+                c += *i;
+                res_it++;
+            }
+            else
+            {
+                res_it = c.insert(res_it, *i);
+                res_it++;
+            }
+            
+        }
+    }
+
+    if(c==a)
+        std::cout << "Yes";
+    else
+        std::cout << "No";
+ }
+
---
+++ yandex1/yandex1.cpp
@@ -0,0 +1,43 @@
+﻿
+#include <iostream>
+#include <string>
+
+bool nik_approved(const std::string& s)
+{
+    if (s.empty())
+        return false;
+    else
+    {
+        bool len_8 = s.length() > 7, dig = false, small_let = false, cap_letter = false;
+        for (auto i : s)
+            if (i >= '0' && i <= '9')
+            {
+                dig = true;
+                break;
+            }
+        for (auto i : s)
+            if (i >= 'a' && i <= 'z')
+            {
+                small_let = true;
+                break;
+            }
+        for (auto i : s)
+            if (i >= 'A' && i <= 'Z')
+            {
+                cap_letter = true;
+                break;
+            }
+        return len_8 && small_let && cap_letter;
+    }
+}
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    if(nik_approved(s))
+        std::cout << "YES";
+    else
+        std::cout << "NO";
+    return 0;
+}
