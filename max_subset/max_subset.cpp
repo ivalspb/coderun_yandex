ef16f9e29fee734b10a16b283d7b86a691a8a2e8
ival <ival.spb@gmail.com>
2024-05-01 10:34:37 +0300

    ноп списаное

--- max_subset/max_subset.cpp
+++ max_subset/max_subset.cpp
@@ -1,60 +1,65 @@
-﻿//Даны две последовательности, требуется найти и вывести их наибольшую общую подпоследовательность.
-//
-
-#include <iostream>
-#include <vector>
+﻿#include <iostream>
+#include <cmath>
 #include <algorithm>
+#include <vector>
 
-int main()
-{
-    size_t N, M;
-    std::cin >> N;
-    std::vector<int>n_set(N);//<value,index>
-    
-    for (size_t i = 0; i < N; i++)
-        std::cin >> n_set[i];
-    std::cin >> M;
-    std::vector<int>m_set(M);
-    std::pair<size_t, size_t> max_lenght = { 0,0 };
-    //<номер первого общего элемента в первом множестве, количество общих элементов>
-    std::pair<size_t, size_t> cur_max_lenght = { 0,0 };
+using namespace std;
 
-    for (size_t j = 0; j < M; j++)
-    {
-        std::cin >> m_set[j];
-        auto i = std::find(n_set.begin()+cur_max_lenght.first+ cur_max_lenght.second,n_set.end(), m_set[j]);//нужно учесть повторы!
-        if (i == n_set.end()&&(cur_max_lenght.first + cur_max_lenght.second))
-        {
-            i= std::find(n_set.begin() + cur_max_lenght.first + cur_max_lenght.second-1, n_set.end(), m_set[j]);
-        }
-        if (i != n_set.end())
-        {
-            if (!cur_max_lenght.second)//first el unity
-            {
-                cur_max_lenght.first = std::distance(n_set.begin(),i);
-                cur_max_lenght.second = 1;
-            }
-            else if(std::distance(n_set.begin(),i)-cur_max_lenght.second== cur_max_lenght.first)
-            {
-                cur_max_lenght.second++;
-            }
-            else if (cur_max_lenght.first<N && n_set[cur_max_lenght.first]== n_set[cur_max_lenght.first+1])
-            {
-                cur_max_lenght.first++;
-                cur_max_lenght.second++;
-            }
-            else
-            {
-                cur_max_lenght.first = std::distance(n_set.begin(),i);
-                cur_max_lenght.second = 1;
-            }
-        }
-        else
-            cur_max_lenght = { 0,0 };
-        if (cur_max_lenght.second > max_lenght.second)
-            max_lenght = cur_max_lenght;
+const int N = 1002;
+
+int x[N], y[N], a[N][N];
+
+int main() {
+
+    int n, m;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> x[i];
     }
-    for(size_t i=0;i<max_lenght.second;i++)
-        std::cout << n_set[max_lenght.first+i] <<" ";
-}
+    cin >> m;
+    for (int i = 1; i <= m; i++) {
+        cin >> y[i];
+    }
 
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (x[i] == y[j]) {
+                a[i][j] = 1 + a[i - 1][j - 1];
+            }
+            else {
+                a[i][j] = max(a[i - 1][j], a[i][j - 1]);
+            }
+        }
+    }
+    cout << endl; cout << endl;
+    for (auto i =1; i<=n; i++)
+    {
+        for (auto j = 1; j <= m; j++) cout << a[i][j] << " ";
+        cout << endl;
+    }
+
+    vector<int> v;
+
+    int i = n, j = m;
+
+    while (i && j) {
+        if (x[i] == y[j]) {
+            v.push_back(x[i]);
+            i--;
+            j--;
+        }
+        else if (a[i - 1][j] == a[i][j]) {
+            i--;
+        }
+        else {
+            j--;
+        }
+    }
+
+    for (int k = v.size() - 1; k >= 0; k--) {
+        cout << v[k] << ' ';
+    }
+
+    return 0;
+}
