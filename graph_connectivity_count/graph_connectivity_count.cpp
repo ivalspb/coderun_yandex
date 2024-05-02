cbc900866dd9d4ff1595b5e4c5b8609faf7c4430
ival <ival.spb@gmail.com>
2024-05-02 08:05:25 +0300

    счетчик связанностей не работает

---
+++ graph_connectivity_count/graph_connectivity_count.cpp
@@ -0,0 +1,58 @@
+﻿
+#include <iostream>
+#include <map>
+#include <set>
+#include <stack>
+
+using namespace std;
+
+//find one connectivity part of graph and mark it visited and push to stack and count++ it
+bool mark_connetivity(map<size_t, set<size_t>>& verteces, const size_t& vertex, map<size_t, bool>& visited, set<size_t>& connecitivty_set)
+{
+    if (!visited[vertex])
+    {
+        visited[vertex] = true;
+        connecitivty_set.insert(vertex);
+        for (const auto& i : verteces[vertex])
+        {
+            if (mark_connetivity(verteces, i, visited, connecitivty_set)) return true;
+        }
+    }
+    else return false;
+}
+
+int main()
+{
+    size_t N_verteces, M_edges;
+    cin >> N_verteces >> M_edges;
+    map<size_t, set<size_t>>verteces;
+    for (size_t i = 0; i < M_edges; i++)
+    {
+        size_t vertex1, vertex2;
+        cin >> vertex1 >> vertex2;
+        verteces[vertex1].insert(vertex2);
+        verteces[vertex2].insert(vertex1);
+    }
+    map<size_t, bool>v_visited;
+    stack<set<size_t>> connecivity_stack;
+    size_t connectivity_count = 0;
+    for (size_t i = 1; i <= N_verteces; i++)
+    {
+        set<size_t> connectivity_set;
+        if (mark_connetivity(verteces, i, v_visited, connectivity_set))
+        {
+            connecivity_stack.push(connectivity_set);
+            connectivity_count++;
+        }
+    }
+
+    cout << connectivity_count;
+    while (!connecivity_stack.empty())
+    {
+        set<size_t> connectivity_set=connecivity_stack.top();
+        connecivity_stack.pop();
+        cout << "\n"<<connectivity_set.size() << endl;
+        for (const auto& i : connectivity_set)
+            cout << i << " ";
+    }
+}
