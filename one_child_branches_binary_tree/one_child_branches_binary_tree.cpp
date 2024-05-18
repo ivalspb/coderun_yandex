66c84051ad34bcc70b38b53da8e10c932e617ba9
ival <ival.spb@gmail.com>
2024-05-18 18:37:21 +0300

    btree nullptr not checked

--- one_child_branches_binary_tree/one_child_branches_binary_tree.cpp
+++ one_child_branches_binary_tree/one_child_branches_binary_tree.cpp
@@ -19,48 +19,69 @@ using namespace std;
 
 struct btree
 {
-struct btree_node
-{
-    int val;
-    btree_node* parent, *left, *right;
-};
-btree_node Head;
-
-bool greater_branch(int x, btree_node* vertex)
-{
-    if (x > vertex->val) return false;
-    if (vertex->left == nullptr && vertex->right == nullptr) return true;
-    if (!vertex->left) return greater_branch(x, vertex->right);
-    if (!vertex->right) return greater_branch(x, vertex->left);
-    return greater_branch(x, vertex->left) && greater_branch(x, vertex->right);
-}
-
-bool less_branch(int x, btree_node* vertex)
-{
-    if (x < vertex->val) return false;
-    if (vertex->left == nullptr && vertex->right == nullptr) return true;
-    if (!vertex->left) return less_branch(x, vertex->right);
-    if (!vertex->right) return less_branch(x, vertex->left);
-    return less_branch(x, vertex->left) && less_branch(x, vertex->right);
-}
-
-
-void ins_btree(int x, btree_node* vertex)
-{
-    if (x != vertex->val)
+    struct btree_node
     {
-        btree_node* new_node = new btree_node;
-        if (x < vertex->val && less_branch(x, vertex->left))
+        int val;
+        btree_node* parent, *left, *right;
+    };
+    btree_node Head;
+
+    bool greater_branch(int x, btree_node* vertex)
+    {
+        if (x > vertex->val) return false;
+        if (vertex->left == nullptr && vertex->right == nullptr) return true;
+        if (!vertex->left) return greater_branch(x, vertex->right);
+        if (!vertex->right) return greater_branch(x, vertex->left);
+        return greater_branch(x, vertex->left) && greater_branch(x, vertex->right);
+    }
+
+    bool less_branch(int x, btree_node* vertex)
+    {
+        if (x < vertex->val) return false;
+        if (vertex->left == nullptr && vertex->right == nullptr) return true;
+        if (!vertex->left) return less_branch(x, vertex->right);
+        if (!vertex->right) return less_branch(x, vertex->left);
+        return less_branch(x, vertex->left) && less_branch(x, vertex->right);
+    }
+
+
+    void ins_btree(int x, btree_node* vertex)
+    {
+        if (x != vertex->val)
         {
-            //insert
-            new_node->parent = vertex->parent;
-            new_node->left = vertex->left;
-            new_node->right = vertex;
-            vertex->left->parent = new_node;
-            vertex->parent = new_node;
+            if (x < vertex->val)
+            {
+                if (less_branch(x, vertex->left))
+                {
+                    btree_node* new_node = new btree_node;
+                    //insert
+                    new_node->parent = vertex->parent;
+                    new_node->left = vertex->left;
+                    new_node->right = vertex;
+                    vertex->left->parent = new_node;
+                    vertex->parent = new_node;
+                }
+                else
+                {
+                    ins_btree(x, vertex->left);
+                }
+            }
+            else// if (x > vertex->val)
+            {
+                if (greater_branch(x, vertex->right))
+                {
+                    btree_node* new_node = new btree_node;
+                    new_node->parent = vertex->parent;
+                    new_node->left = vertex;
+                    new_node->right = vertex->right;
+                    vertex->right->parent = new_node;
+                    vertex->parent = new_node;
+                }
+                else
+                    ins_btree(x, vertex->right);
+            }
         }
     }
-}
 
 };
 
@@ -68,12 +89,12 @@ void ins_btree(int x, btree_node* vertex)
 int main()
 {
     int num;
-    vector<btree_node>v;
+    btree btr;
     do
     {
         cin >> num;
         if (num) 
-            //v.push_back(num);
+            btr.ins_btree(num,&btr.Head);
     } while (num);
     set<int> res;
 
