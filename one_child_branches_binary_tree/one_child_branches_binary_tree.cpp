bcf86d084f8fdff6e182be5f21a4acd7c3a1d1f9
ival <ival.spb@gmail.com>
2024-05-18 08:05:16 +0300

    btree insert?

--- one_child_branches_binary_tree/one_child_branches_binary_tree.cpp
+++ one_child_branches_binary_tree/one_child_branches_binary_tree.cpp
@@ -1,4 +1,7 @@
 /*
+* 124. Вывод листьев
+
+* 
 Для бинарного дерева поиска, построенного на заданных элементах, выведите список всех вершин, имеющих только одного ребёнка, в порядке возрастания.
 
 Формат ввода
@@ -9,8 +12,62 @@
 */
 
 #include <iostream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+struct btree
+{
+struct btree_node
+{
+    int val;
+    btree_node* parent, *left, *right;
+};
+btree_node Head;
+
+bool greater_branch(int x, btree_node* vertex)
+{
+    if (x > vertex->val) return false;
+    if (vertex->left == nullptr && vertex->right == nullptr) return true;
+    if (!vertex->left) return greater_branch(x, vertex->right);
+    if (!vertex->right) return greater_branch(x, vertex->left);
+    return greater_branch(x, vertex->left) && greater_branch(x, vertex->right);
+}
+
+bool less_branch(int x, btree_node* vertex)
+{
+    if (x < vertex->val) return false;
+    if (vertex->left == nullptr && vertex->right == nullptr) return true;
+    if (!vertex->left) return less_branch(x, vertex->right);
+    if (!vertex->right) return less_branch(x, vertex->left);
+    return less_branch(x, vertex->left) && less_branch(x, vertex->right);
+}
+
+
+void ins_btree(int x, btree_node* vertex)
+{
+    if (x != vertex->val)
+    {
+        btree_node* new_node = new btree_node;
+        if()
+    }
+}
+
+};
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int num;
+    vector<btree_node>v;
+    do
+    {
+        cin >> num;
+        if (num) 
+            //v.push_back(num);
+    } while (num);
+    set<int> res;
+
+    return 0;
 }
