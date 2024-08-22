54ca6f8fe8f96c15eb91f6724e4f3b4d24a03d3a
ivalspb <ival.spb@gmail.com>
2024-08-22 18:00:31 +0300

    две команды зачет

---
+++ две команды/две команды.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    size_t A, B, N, A_max, B_min;
+    cin >> A >> B >> N;
+    A_max = A;
+    B_min = B / N;
+    if (B % N) 
+        B_min++;
+    if (A_max > B_min) cout << "Yes";
+    else cout << "No";
+}
+
