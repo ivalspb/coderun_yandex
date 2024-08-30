1caa9c959ed1c54ddfadb72eb56cba43db23dbfd
ivalspb <ival.spb@gmail.com>
2024-08-30 18:43:46 +0300

    ближайшая остановка неправильно граничные запросы больше всех остановок

--- 423. Ближайшая остановка/423. Ближайшая остановка.cpp
+++ 423. Ближайшая остановка/423. Ближайшая остановка.cpp
@@ -10,21 +10,22 @@ size_t get_stop(map<int,pair<size_t,size_t>>& s, int req)
     
     if (s_ind != s.end())
         return s_ind->second.first;//минимальная среди остановок совпадающих с запросом
-    
+    \
     auto s_upper = s.upper_bound(req);
     auto s_lower = s_upper;
     if (s_lower != s.begin())
         --s_lower;
     auto s_min = s_upper!=s.end()?(s_upper->first - req < s_lower->first - req ? s_upper : s_lower):s_lower;
     
-    if ((s_min->second.first != s_min->second.second) or ((s_upper->first - req == s_lower->first - req)&&(s_upper!=s_lower)))//несколько остановок с одинаковым расстоянием
+    if ((s_min->second.first != s_min->second.second) or 
+        ((s_upper!=s.end())&&(s_upper!=s_lower) && (s_upper->first - req == s_lower->first - req)))//несколько остановок с одинаковым расстоянием
     {
-        if (s_min != --s.end() && s_min!= s.begin())//остановки и слева и справа
-            return (--s.lower_bound(req))->second.second;
-        else if (s.lower_bound(req) == s.end())//остановки только слева
-            return (--s.lower_bound(req))->second.second;
+        if (abs(s_upper->first - req) == abs(s_lower->first - req))//остановки и слева и справа одинаково минимально удалены от запроса
+            return s_lower->second.second;
+        else if (abs(s_lower->first - req) < abs(s_upper->first - req))//остановки минимально удаленные от запроса только слева
+            return s_lower->second.second;
         else//остановки только справа
-            return s.begin()->second.first;
+            return s_upper->second.first;
     }
     else
         return s_min->second.first;
